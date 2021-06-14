99218ae76c635f28749377703966595522f903aa
IanAquino <ianpedroaquino@gmail.com>
2021-06-14 23:00:02 +0100

    Revert "Function loyalty and field email"
    This reverts commit c88723a21c9823d7a47cc81d4d97627ed589e30c.

---
+++ var/cache/templates/bright_theme/970db53f20c81aa8e9d1df427de146e4af3956b2.string.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.21, created on 2021-06-15 00:57:30
+         compiled from "970db53f20c81aa8e9d1df427de146e4af3956b2" */ ?>
+<?php /*%%SmartyHeaderCode:97077965960c7d0cae395d9-16717871%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '970db53f20c81aa8e9d1df427de146e4af3956b2' => 
+    array (
+      0 => '970db53f20c81aa8e9d1df427de146e4af3956b2',
+      1 => 0,
+      2 => 'string',
+    ),
+  ),
+  'nocache_hash' => '97077965960c7d0cae395d9-16717871',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21',
+  'unifunc' => 'content_60c7d0cae3ae57_67568134',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_60c7d0cae3ae57_67568134')) {function content_60c7d0cae3ae57_67568134($_smarty_tpl) {?><ul>
+    <li>3993 Hanover Street<br/>New York, NY 10016
+</li>
+    <li><bdi>+1 917-722-7425</bdi> (call is free)</li>
+    <li><bdi>10 am — 6 pm</bdi><br/>
+Monday - Friday</li>
+    <li><a class="ty-exception__links-a greeny" href="mailto:sales@example.com">sales@example.com</a></li>
+
+    <li><a href="<?php echo htmlspecialchars(fn_url("pages.view?page_id=20"), ENT_QUOTES, 'UTF-8');?>
+" class="ty-exception__links-a">About company</a></li>
+
+</ul>
+
+<?php }} ?>
---
+++ var/cache/templates/bright_theme/c30a02260f0d832ee90818726686ea6ec86997c3.string.php
@@ -0,0 +1,43 @@
+<?php /* Smarty version Smarty-3.1.21, created on 2021-06-15 00:57:30
+         compiled from "c30a02260f0d832ee90818726686ea6ec86997c3" */ ?>
+<?php /*%%SmartyHeaderCode:124960190460c7d0cad44179-02294661%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'c30a02260f0d832ee90818726686ea6ec86997c3' => 
+    array (
+      0 => 'c30a02260f0d832ee90818726686ea6ec86997c3',
+      1 => 0,
+      2 => 'string',
+    ),
+  ),
+  'nocache_hash' => '124960190460c7d0cad44179-02294661',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21',
+  'unifunc' => 'content_60c7d0cad45442_52431545',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_60c7d0cad45442_52431545')) {function content_60c7d0cad45442_52431545($_smarty_tpl) {?><?php
+\Tygh\Languages\Helper::preloadLangVars(array('get_social'));
+?>
+<div class="ty-social-link-block"><h3 class="ty-social-link__title"><?php echo $_smarty_tpl->__("get_social");?>
+</h3>
+
+<div class="ty-social-link facebook">
+    <a href="http://www.facebook.com/pages/CS-Cart/156687676230"><i class="ty-icon-facebook ty-icon-moon-facebook"></i></a>
+</div>
+
+<div class="ty-social-link instagram">
+    <a href="https://www.instagram.com"><i class="ty-icon-instagram ty-icon-moon-instagram"></i></a>
+</div>
+
+<div class="ty-social-link twitter">
+    <a href="https://twitter.com/cscart"><i class="ty-icon-twitter ty-icon-moon-twitter"></i></a>
+</div>
+
+<div class="ty-social-link youtube">
+    <a href="https://www.youtube.com/user/cscartvideos"><i class="ty-icon-youtube ty-icon-moon-youtube"></i></a>
+</div></div>
+<?php }} ?>
